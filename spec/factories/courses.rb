a4076c78fa076146eb27ba17837183c1fd95ab02
Gabriel Barboza <gabrielmbarboza@gmail.com>
2020-10-03 00:49:14 -0300

    Alterei o atributo do Faker em name na factory de Courses

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :course do
-    name { Faker::Educator.course_name}
+    name { Faker::Educator.subject }
     kind { %w[In-class On-line].sample }
     level { Faker::Educator.degree }
     shift { %w[First\ shift Second\ shift Third\ shift].sample }
